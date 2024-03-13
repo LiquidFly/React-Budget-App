ab5e4209aeae50e74c4234cff334e1b58cb93d54
Shravan_Khandelwal <102209703+LiquidFly@users.noreply.github.com>
2024-03-13 22:51:33 +0530

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -3,9 +3,3 @@ import { render, fireEvent, createTest } from 'vitest';
 import App from './App';
 
 const test = createTest();
-
-test('renders title correctly', () => {
-  const { getByText } = render(<App />);
-  const titleElement = getByText(/Budget Tracker/i);
-  expect(titleElement).not.toBeNull();
-});
